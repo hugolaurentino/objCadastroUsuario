62ac10afdbac84389d0bd09c60f5d3f1e748f367
hugo <hugof16silva@gmail.com>
2023-10-24 20:59:29 -0300

    primeiro commit

---
+++ objCadastroUsuario.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: 'Sergipe Beatriz',
+    numero: 4650,
+    complemento: 'casa A',
+    bairro: 'Cidade Kemel',
+    cep: '08792 - 150',
+    cidade: 'Ferraz',
+    estado: 'São Paulo',
+    pais: 'Brazil'
+}
+
+const usuario = {
+    nome: 'Hugo',
+    email: 'hugo@gmail.com',
+    telefone: 11999999999,
+    data_nascimento: '16121982',
+    endereco
+}
+
+console.log(usuario);
